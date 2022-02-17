2734562d3ad29333a0d023feb958b05d3d896d41
MaverickQuill55 (VS Code) <VScode@toxik.cf>
2022-02-17 11:09:21 -0500

    add actuall functionality to the bot, remove command will be added later

---
+++ commands/addrule.js
@@ -0,0 +1,61 @@
+const { PrismaClient } = require('@prisma/client');
+/**
+ * @type {PrismaClient}
+ */
+const db = require('#db');
+const lib = require('#lib')
+const { Client, CommandInteraction } = require('discord.js')
+module.exports = {
+    name: 'addrule',
+    description: 'Add a new rule',
+    options: [
+        {
+            type: 3,
+            name: "title",
+            description: "A title for your rule",
+            required: true
+        },
+        {
+            type: 3,
+            name: "description",
+            description: "A description for your rule",
+            required: true
+        }
+    ],
+    limits: {
+        owner: false,
+        permissions: ["MANAGE_SERVER"]
+    },
+    /**
+     * 
+     * @param {Client} client 
+     * @param {CommandInteraction} interaction 
+     */
+    run: (client, interaction) => {
+        (async() => {
+            const guildid = interaction.guildId;
+            let ok = false
+            if(!await db.server.findFirst({ where: { id: guildid } })) return interaction.reply(`This server is not registered!`)
+            const guildata = await db.server.findFirst({ where: { id: guildid }, select: { rules: true } })
+
+            db.rule.create({
+                data: {
+                    Number: guildata?.rules?.length + 1,
+                    Title: interaction.options.getString("title"),
+                    Info: interaction.options.getString("description"),
+                    serverId: guildid
+                }
+            }).catch(e=>{
+                interaction.reply('command ran into an error')
+                console.error(e)
+                ok = true
+            }).finally(()=>{
+                db.$disconnect()
+                lib.editrules(guildid, client)
+                if(!ok) {interaction.reply('Done!').catch(()=>{
+                    console.error("An error ocurred")
+                })}
+            })
+        })()
+    }
+}
---
+++ lib.js
@@ -0,0 +1,47 @@
+const { PrismaClient } = require('@prisma/client');
+/**
+ * @type {PrismaClient}
+ */
+const db = require('#db');
+const { Client, MessageEmbed } = require('discord.js');
+
+/**
+ * @description Edits the server rules messah
+ * @param {String} svid
+ * @param {Client} client
+ */
+module.exports.editrules = function(svid, client) {
+    (async()=>{
+        const server = await db.server.findFirst({
+            where: {
+                id: svid
+            },
+            select: {
+                rules: true,
+                ruleChannelId: true,
+                ruleEmbedMessage: true
+            }
+        })
+        const guild = client.guilds.cache.get(svid)
+        const channel = guild.channels.cache.get(server.ruleChannelId)
+        const message = await channel.messages.fetch(server.ruleEmbedMessage)
+        console.log(message)
+        message.edit({ embeds: [
+            new MessageEmbed({
+                title: "Rules",
+                fields: [
+                    {
+                       name: "Rule 0.",
+                       value: "Follow the rules."
+                    },
+                    ...server.rules.map(r=>{
+                        return {
+                            name: `Rule ${r.Number}.${r.Title?'':` ${r.Title}`}`,
+                            value: r.Info
+                        }
+                    })
+                ]
+            })
+        ]})
+    })()
+}
