ae3c3c77957508deeb128bd5109413cfb330df0c
MaverickQuill55 (VS Code) <VScode@toxik.cf>
2022-02-17 11:13:06 -0500

    Fix bot activity

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,15 @@
+const { Client } = require("discord.js");
 module.exports = {
     name: "ready",
     settings: {
         once: false
     },
+    /**
+     * 
+     * @param {Client} client 
+     */
     run: (client) => {
         console.log("I'm online!");
-        client.user.setActivity("Listening to slash commands");
+        client.user.setActivity("The rules", { type: "LISTENING" });
     }
 }
