bf6711f570e6d91288bd3e38897a8a7d61d8dd7b
MaverickQuill55 (VS Code) <VScode@toxik.cf>
2022-02-17 18:28:58 -0500

    do more things

---
+++ commands/updaterules.js
@@ -0,0 +1,23 @@
+const lib = require('#lib')
+const { Client, CommandInteraction } = require('discord.js')
+module.exports = {
+    name: 'updates',
+    description: 'Update rules embed',
+    limits: {
+        owner: false,
+        permissions: ["MANAGE_SERVER"]
+    },
+    /**
+     * 
+     * @param {Client} client 
+     * @param {CommandInteraction} interaction 
+     */
+    run: (client, interaction) => {
+        (async() => {
+            const guildid = interaction.guildId;
+            await interaction.deferReply();
+            lib.editrules(guildid, client, true)
+            await interaction.editReply("Done!");
+        })()
+    }
+}
