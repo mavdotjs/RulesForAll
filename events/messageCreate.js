3abb6f62d7cc2ccff463d2063a073047c2d2c906
MaverickQuill55 (VS Code) <VScode@toxik.cf>
2022-02-20 11:39:03 -0500

    add presence updates

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -16,17 +16,15 @@ module.exports = {
      */
     run: (client, message) => {
         (async()=>{
-            if(!message.author.bot) return console.log("test 1 fail");
-            if(!(message.author.id === client.user.id)) return console.log("test 2 fail");
+            if(!message.author.bot) return;
+            if(!(message.author.id === client.user.id)) return;
             const guildId = message.guildId;
             await new Promise(r => setTimeout(r, 2000))
             const server = await db.server.findFirst({ where: { id: guildId } })
-            if(!server) return console.log("test 3 fail");
+            if(!server) return;
             if(message.id === server.ruleEmbedMessage) {
-                console.log("rule embed created")
                 await message.pin()
             } else if(message.id === server.ruleAcceptMessage) {
-                console.log("accept rule embed created")
                 await message.react("✅")
             }
         })().then(()=>{
--- events/ready.js
+++ events/ready.js
@@ -10,6 +10,22 @@ module.exports = {
      */
     run: (client) => {
         console.log("I'm online!");
-        client.user.setActivity("The rules", { type: "LISTENING" });
+        const presence = [
+            { type: 'PLAYING', name: 'with pixelbot' },
+            { type: 'LISTENING', name: 'the rules' }
+        ]
+        let current_presence = 0
+        function update() {
+            current_presence++;
+            const random = Math.random();
+            const random2 = random * presence.length 
+            const randomFloor = Math.floor(random2);
+            const activity = presence[randomFloor]
+            client.user.setPresence({ activities: [activity], status: 'idle' });
+        }
+        setInterval(update, 16000)
+        client.guilds.cache.forEach(guild => {
+            console.log(`${guild.name} | ${guild.id}`);
+        })
     }
 }
