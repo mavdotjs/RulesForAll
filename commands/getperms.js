dec6a1ff5d8c11825788e02bef8f77a6c20fe6ff
MaverickQuill55 <VScode@toxik.cf>
2022-02-24 19:44:27 -0500

    add more debug commands

---
+++ commands/getperms.js
@@ -0,0 +1,31 @@
+const { Client, CommandInteraction } = require('discord.js');
+
+
+module.exports = {
+    name: "perms",
+    description: "Gets the permissions for a channel, for debugging",
+    options: [
+        {
+            type: 7,
+            name: "channel",
+            description: "The channel to check for.",
+            required: true
+        },
+    ],
+    limits: {
+        owner: true,
+    },
+    /**
+     * 
+     * @param {Client} client 
+     * @param {CommandInteraction} interaction 
+    */
+    run: (client, interaction) => {
+        const channel = interaction.options.getChannel('channel');
+        console.log(interaction.guild.me.permissionsIn(channel.id).toArray())
+        async(()=>{
+            await interaction.deferReply()
+            await interaction.editReply("Check logs!")
+        })()
+    }
+}
