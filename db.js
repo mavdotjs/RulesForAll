8622e1bc4398019f0a7d0e3fc3e089c7bcf203d4
maverickquill55 <consorceo300@icloud.com>
2023-01-20 07:57:08 -0500

    reset

--- db.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client')
-
-const prisma = new PrismaClient()
-
-
-/**
- * @type {PrismaClient}
- */
-module.exports = prisma;
--- prisma/migrations/20220217020007_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `ruleEmbedMessage` to the `Server` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Server" ADD COLUMN     "ruleEmbedMessage" TEXT NOT NULL;
